76538b3e97bdf390b2e24f2631fc327bc98dd4c9
shivadharmala <shivaprasadreddy.dharmala@gmail.com>
2025-04-12 18:18:59 +0530

    Code Changes

--- src/main/java/com/tech/society/api_gateway/config/GatewayRoutesConfig.java
+++ src/main/java/com/tech/society/api_gateway/config/GatewayRoutesConfig.java
@@ -1,19 +1,23 @@
 package com.tech.society.api_gateway.config;
 
+import org.slf4j.Logger;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 
 @Configuration
 public class GatewayRoutesConfig {
 
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+
+
         return builder.routes()
             .route("resident-service", r -> r.path("/resident/**")
                 .filters(f -> f.stripPrefix(1))
-                .uri("http://localhost:8081"))
+                .uri("http://localhost:9090/"))
             .build();
     }
 }
